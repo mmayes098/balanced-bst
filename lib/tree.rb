7db58823c044061a6a38404943913cfcf9c342b4
mmayes098 <mayes.matthewj@gmail.com>
2021-04-23 14:56:10 -0400

    Added traversal methods and height method

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,7 +2,7 @@ require_relative "node"
 
 class Tree
   attr_accessor :root
-  
+
   def initialize(array)
     @sorted = array.sort.uniq
     @root = build_tree(@sorted, 0, @sorted.length - 1)
@@ -36,5 +36,66 @@ class Tree
     else
       puts "That value doesn't exist in the tree!"
     end
+  end
+
+  def find(value, root = @root)
+    return root if root.data == value
+
+    if value < root.data
+      find(value, root.leftChild)
+    elsif value > root.data
+      find(value, root.rightChild)
+    end
+  rescue NoMethodError
+    puts "Value not found!"
+  end
+
+  def level_order(root = @root)
+    queue = [root]
+    values = []
+    until queue.empty?
+      root = queue.shift
+      values << root.data
+      queue.push(root.leftChild) unless root.leftChild.nil?
+      queue.push(root.rightChild) unless root.rightChild.nil?
+    end
+
+    values
+  end
+
+  def inorder(root = @root, values = [])
+    return if root.nil?
+
+    inorder(root.leftChild, values)
+    values << root.data
+    inorder(root.rightChild, values)
+
+    values
+  end
+
+  def preorder(root = @root, values = [])
+    return if root.nil?
+
+    values << root.data
+    preorder(root.leftChild, values)
+    preorder(root.rightChild, values)
+
+    values
+  end
+
+  def postorder(root = @root, values = [])
+    return if root.nil?
+
+    postorder(root.leftChild, values)
+    postorder(root.rightChild, values)
+    values << root.data
+
+    values
+  end
+
+  def height(node = @root)
+    return -1 if node.nil?
+
+    [height(node.leftChild), height(node.rightChild)].max + 1
   end
 end
